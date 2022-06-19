617fc1e094b629fcba1628e6177d6fbfc7989289
Iljuhen <102986980+Iljuhen@users.noreply.github.com>
2022-06-20 00:40:24 +0300

    Написание программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Clear();
+
+string[] firstArr = new string[]
+{ "cat", "flag", "12345", "soft","yes", "csharp", "$)", "5" };
+
+string[] secArr = new string[firstArr.Length];
+
+Console.WriteLine("Задан массив:  ");
+Console.WriteLine();
+Console.WriteLine(PrintArray(firstArr));
+Console.WriteLine();
+SecArray(secArr);
+Console.WriteLine("новый массив:  ");
+Console.WriteLine();
+Console.WriteLine(PrintArray(secArr));
+Console.WriteLine();
+
+void SecArray(string[] secArr)
+{
+    int sizeIndex = 0;
+    for (int i = 0; i < firstArr.Length; i++)
+    {
+
+        if (firstArr[i].Length <= 3)
+        {
+            secArr[sizeIndex] = firstArr[i];
+            sizeIndex++;
+        }
+
+    }
+return;
+}
+
+
+string PrintArray(string[] array)
+{
+    string res = string.Empty;
+    res = "|";
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += $"{array[i]} | ";
+    }
+    res += " ";
+    return res ;
+}
+
